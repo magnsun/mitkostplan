39343f41495c3b1e9e5829efaf6cadfd23639c19
magnus <magnus01@live.dk>
2024-05-07 09:40:39 +0200

    Initial commit

---
+++ src/main/java/com/kostplan/mitkostplan/MitkostplanApplication.java
@@ -0,0 +1,13 @@
+package com.kostplan.mitkostplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitkostplanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MitkostplanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kostplan/mitkostplan/MitkostplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kostplan.mitkostplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitkostplanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
