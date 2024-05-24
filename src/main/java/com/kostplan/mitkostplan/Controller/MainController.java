76ff3bb28af30dab16d1b48c42ff43d0adde7d56
Murel02 <olimurel@gmail.com>
2024-05-24 23:46:01 +0200

    p

--- src/main/java/com/kostplan/mitkostplan/Controller/MainController.java
+++ src/main/java/com/kostplan/mitkostplan/Controller/MainController.java
@@ -1,11 +1,14 @@
 package com.kostplan.mitkostplan.Controller;
 
+import com.kostplan.mitkostplan.Entity.User;
 import com.kostplan.mitkostplan.Service.UseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
 
 @Controller
 @RequestMapping("/main")
@@ -20,8 +23,9 @@ public class MainController {
 
 
     @GetMapping()
-    public String showMain(Model model){
-
+    public String showMain(Model model, Principal principal){
+        User user = useCase.getUserByMail(principal.getName());
+        model.addAttribute("userInfo", user);
         return "main";
     }
 
